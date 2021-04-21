49be2eb0d57566280eaacfc23c8a6b7c1d400c67
Tsu_Sem <tsukasa0171015@gmail.com>
2021-04-21 19:54:09 +0900

    add: navigation objects on DetailGroupFragment
    still have below tasks
    -fixing networking-error

--- app/src/main/java/com/example/droidsoftthird/GroupDetailFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/GroupDetailFragment.kt
@@ -1,15 +1,18 @@
 package com.example.droidsoftthird
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil.inflate
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.droidsoftthird.databinding.FragmentGroupDetailBinding
-import com.example.droidsoftthird.databinding.FragmentPagerRecommendBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 
 class GroupDetailFragment : Fragment() {
@@ -22,12 +25,33 @@ class GroupDetailFragment : Fragment() {
     ): View? {
 
         val binding: FragmentGroupDetailBinding = inflate(
-            inflater, R.layout.fragment_group_detail, container, false)
+            inflater, R.layout.fragment_group_detail, container, false
+        )
+
+        //-----ViewObjects for Navigation
+        val layout: CollapsingToolbarLayout = binding.collapsingToolbarLayout
+        val toolbar: Toolbar = binding.materialToolbar
+        toolbar.title = " "//TODO ツールバータイトルの良い表示方法を検討する。
+
+        //-----NavUI Objects
+        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        val appBarConfiguration = AppBarConfiguration.Builder(navController.getGraph()).build()
+
+        //-----Setup for NavigationUI
+        NavigationUI.setupWithNavController(
+            layout,
+            toolbar,
+            navController,
+            appBarConfiguration
+        )
+
         val repository = GroupRepository()
         val arguments = GroupDetailFragmentArgs.fromBundle(requireArguments())
-        val viewModelFactory = GroupDetailViewModelFactory(arguments.groupId,repository)
+        val viewModelFactory = GroupDetailViewModelFactory(arguments.groupId, repository)
         val viewModel = ViewModelProvider(
-            this, viewModelFactory).get(GroupDetailViewModel::class.java)
+            this, viewModelFactory
+        ).get(GroupDetailViewModel::class.java)
 
         //DONE GroupDetailViewModelのコーディング
         //DONE GroupDetailViewModelFactoryのコーディング
