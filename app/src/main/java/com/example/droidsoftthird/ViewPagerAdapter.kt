09aa80b454117706e56750d8cdffceb031a74d3f
Tsu_Sem <tsukasa0171015@gmail.com>
2021-06-27 22:09:41 +0900

    update: updated HomeClasses & AddGroupClasses
    upgrade layout, fix a few bugs, activate progress bar etc...

--- app/src/main/java/com/example/droidsoftthird/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/droidsoftthird/ViewPagerAdapter.kt
@@ -5,17 +5,15 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import java.lang.IndexOutOfBoundsException
 
-const val PAGE_COUNT = 3
+const val PAGE_COUNT = 2
 const val RECOMMEND_PAGE_INDEX = 0
-const val SCHEDULE_PAGE_INDEX = 1
-const val MAP_PAGE_INDEX = 2
+const val MAP_PAGE_INDEX = 1
 
 
 class HomeViewPagerAdapter(fragment:Fragment): FragmentStateAdapter(fragment){
 
     private val tabFragmentCreators: Map<Int, () -> Fragment> = mapOf(
         RECOMMEND_PAGE_INDEX to {RecommendPagerFragment()},
-        SCHEDULE_PAGE_INDEX to {SchedulePagerFragment()},
         MAP_PAGE_INDEX to {MapPagerFragment()}
     )
 
