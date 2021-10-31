2b1baef452cb315d35dafe7325f4404094b0e677
tsemb012 <tsukasa0171015@gmail.com>
2021-10-31 17:35:49 +0900

    add: layout of ScheduleRegisteredFragment.kt

--- app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredFragment.kt
@@ -1,6 +1,38 @@
 package com.example.droidsoftthird
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.droidsoftthird.databinding.FragmentScheduleRegisteredBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.wada811.databinding.dataBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class ScheduleRegisteredFragment:Fragment() {
+@AndroidEntryPoint
+class ScheduleRegisteredFragment: Fragment(R.layout.fragment_schedule_registered) {
+
+   // @Inject lateinit var scheduleRegisteredViewModelAssistedFactory: ScheduleRegisteredViewModel.Factory
+    private val viewModel: ScheduleRegisteredViewModel by viewModels()
+    private val binding: FragmentScheduleRegisteredBinding by dataBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        class DayViewContainer(view: View): ViewContainer(view) {
+            lateinit var day: CalendarDay
+
+        }
+    }
+
 }
