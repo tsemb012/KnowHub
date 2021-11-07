da2a1c19062dac03d4943d207eaa42826b3d1e44
tsemb012 <tsukasa0171015@gmail.com>
2021-11-07 13:02:24 +0900

    add: observe method in ScheduleRegisteredFragment.kte

--- app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredFragment.kt
@@ -1,12 +1,16 @@
 package com.example.droidsoftthird
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.droidsoftthird.databinding.FragmentScheduleRegisteredBinding
+import com.example.droidsoftthird.model.LoadState
+import com.example.droidsoftthird.model.SchedulePlan
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.ui.ViewContainer
 import com.wada811.databinding.dataBinding
@@ -19,6 +23,12 @@ class ScheduleRegisteredFragment: Fragment(R.layout.fragment_schedule_registered
    // @Inject lateinit var scheduleRegisteredViewModelAssistedFactory: ScheduleRegisteredViewModel.Factory
     private val viewModel: ScheduleRegisteredViewModel by viewModels()
     private val binding: FragmentScheduleRegisteredBinding by dataBinding()
+    private val progressDialog by lazy {
+        ProgressDialog(requireActivity()).apply {
+            setMessage(getString(R.string.on_progress))
+            setCancelable(false)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +36,36 @@ class ScheduleRegisteredFragment: Fragment(R.layout.fragment_schedule_registered
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.lifecycleOwner = viewLifecycleOwner
+         viewModel.uiModel.observe(viewLifecycleOwner){
+             observeSchedulesState(it.schedulesLoadState)
+         }
 
         class DayViewContainer(view: View): ViewContainer(view) {
             lateinit var day: CalendarDay
 
         }
     }
+
+    private fun observeSchedulesState(state: LoadState) {
+        when (state) {
+            is LoadState.Loading -> progressDialog.show()
+            is LoadState.Loaded<*> -> {
+                progressDialog.dismiss()
+                //TODO ここに成功時の処理を記述する。
+                viewModel.initializeSchedulesState()
+            }
+            is LoadState.Error -> {
+                progressDialog.dismiss()
+                Toast.makeText(requireContext(), state.error.toString(), Toast.LENGTH_SHORT).show()
+                viewModel.initializeSchedulesState()
+            }
+        }
+    }
+
+    private fun showEditTextDialog(state: LoadState.Loaded<*>) {
+
+    }
+
+
 }
