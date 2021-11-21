7103fb150c3a8a42f9836450c16dba940cd9d08d
tsemb012 <tsukasa0171015@gmail.com>
2021-11-21 23:49:03 +0900

    update: RegisteredScheduleFunction

--- app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredViewModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleRegisteredViewModel.kt
@@ -1,42 +1,39 @@
 package com.example.droidsoftthird
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.droidsoftthird.model.LoadState
 import com.example.droidsoftthird.model.toEntity
 import com.example.droidsoftthird.repository.BaseRepositoryImpl
 import com.example.droidsoftthird.repository.BaseRepositoryImpl.Companion.SCHEDULE_REGISTERED_ALL
-import com.example.droidsoftthird.utils.combine
 import kotlinx.coroutines.launch
+import java.time.LocalDate
 
-class ScheduleRegisteredViewModel @ViewModelInject constructor(private val repository: BaseRepositoryImpl): ViewModel() {
+class ScheduleRegisteredViewModel @ViewModelInject constructor(private val repository: BaseRepositoryImpl): ScheduleViewModel(){
 
-    private val _schedulesState: MutableLiveData<LoadState> by lazy { MutableLiveData(LoadState.Initialized) }
-    val uiModel by lazy {
-        combine(
-            ScheduleRegisteredUiModel(),
-            _schedulesState
-        ) { current, schedulesState -> ScheduleRegisteredUiModel(current, schedulesState) }
-    }
 
     fun fetchAllSchedules() { fetchSchedules(SCHEDULE_REGISTERED_ALL) }
-
     private fun fetchSchedules(query: String){
         val job = viewModelScope.launch {
                 repository.getSchedules(query).apply {
                     when(this) {
-                        is Result.Success -> _schedulesState.value = LoadState.Loaded(data.map { it.toEntity() } )
-                        is Result.Failure -> _schedulesState.value = LoadState.Error(exception)
+                        is Result.Success -> {
+                            data.map { it.toEntity() }.let {
+                                schedulesState.value = LoadState.Loaded(it)
+                                selectedEvents.value = it.mapNotNull { scheduleEvent ->
+                                    if (scheduleEvent.date == LocalDate.now()) { scheduleEvent } else null
+                                }
+                            }
+                        }
+                        is Result.Failure -> schedulesState.value = LoadState.Error(exception)
                     }
                 }
         }
-        _schedulesState.value = LoadState.Loading(job)
+        schedulesState.value = LoadState.Loading(job)
         job.start()
     }
 
     fun initializeSchedulesState() {
-        _schedulesState.value = LoadState.Initialized
+        schedulesState.value = LoadState.Initialized
     }
 }
