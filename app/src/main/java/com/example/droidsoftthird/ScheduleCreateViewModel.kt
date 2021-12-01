28faac8af0b3b5c17241574fa8ab33045c7d7c0f
tsemb012 <tsukasa0171015@gmail.com>
2021-12-02 01:08:55 +0900

    add: TimePickerDialog

--- app/src/main/java/com/example/droidsoftthird/ScheduleCreateViewModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleCreateViewModel.kt
@@ -3,12 +3,24 @@ package com.example.droidsoftthird
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.util.*
 
 class ScheduleCreateViewModel: ViewModel() {
 
+    companion object {
+        private const val NOT_SET_JPN = "未設定"
+        private const val NOT_SET_ENG = "Not set"
+    }
+
     var eventName = MutableLiveData<String>()
     var eventComment = MutableLiveData<String>()
+
     var eventDate = MutableLiveData<String>()
+    //TODO データの持ち方に関しては、
+    //TODO GroupAddのところが参考になるはず。そことRPとUiModelを融合させる。
+    //TODO　レイアウトについても青と黄色を反転させた方がおしゃれになりそうだけど。
+    //Constraintにして上部に説明を入れてあげたほうが全体のレイアウトが綺麗になるのでは？
     var eventTime = MutableLiveData<String>()
     var eventLocation = MutableLiveData<String>()
     var eventGroup = MutableLiveData<String>()
