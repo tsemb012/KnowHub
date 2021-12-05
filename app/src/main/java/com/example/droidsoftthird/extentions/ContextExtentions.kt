d83af73c156a83a05f4bf5ecac7dec52864eb532
tsemb012 <tsukasa0171015@gmail.com>
2021-12-05 12:59:39 +0900

    update: update Calendar and Time Dialog to Material / 日時ダイアログをマテリアルデザインにアップデート

---
+++ app/src/main/java/com/example/droidsoftthird/extentions/ContextExtentions.kt
@@ -0,0 +1,43 @@
+package com.example.droidsoftthird.extentions
+
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
+import android.os.LocaleList
+import androidx.core.os.ConfigurationCompat
+import java.util.*
+
+fun Context.createLocalizedContext(locale: Locale): Context {
+    val res = resources
+    val config = Configuration(res.configuration)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        config.setLocales(LocaleList(locale))
+    } else {
+        config.setLocale(locale)
+    }
+    return createConfigurationContext(config)
+}
+
+fun getEnglishContext(baseContext: Context): Context {
+    val currentLocale = Locale.ENGLISH
+    Locale.setDefault(currentLocale)
+    val config = getLocalizedConfiguration(currentLocale)
+    baseContext.createConfigurationContext(config)
+    return baseContext
+}
+
+fun getLocalizedConfiguration(locale: Locale): Configuration {
+    val config = Configuration()
+    return config.apply {
+        config.setLayoutDirection(locale)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            config.setLocale(locale)
+            val localeList = LocaleList(locale)
+            LocaleList.setDefault(localeList)
+            config.setLocales(localeList)
+        } else {
+            config.setLocale(locale)
+        }
+    }
+}
