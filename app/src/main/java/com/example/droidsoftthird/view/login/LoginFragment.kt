1b7958302cac334597d04841f063a6bb3ab35949
tsemb012 <tsukasa0171015@gmail.com>
2022-08-03 01:46:28 +0900

    DST-96 add: row and column of jetpackCompose

--- app/src/main/java/com/example/droidsoftthird/view/login/LoginFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/view/login/LoginFragment.kt
@@ -5,29 +5,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
+import com.example.droidsoftthird.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return ComposeView(requireContext()).apply { 
-            setContent { MessageCard("JetpackCompose") }
+            setContent { MessageCard(Message("JetpackCompose", "Android")) }
         }
     }
+    
+    data class Message(val component: String, val os:String)
 
     @Composable
-    private fun MessageCard(s: String) {
-        Text(text = "Hello $s")
+    private fun MessageCard(msg: Message) {
+        Row{
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "picture",
+            )
+            Column {
+                Text(text = msg.os)
+                Text(text = msg.component)
+            }
+
+        }
     }
 
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(s = "JetPackCompose")
+        MessageCard(msg = Message("JetpackCompose", "Android"))
     }//パラメータを受け取らないコンポーズ可能な関数であること
 }
