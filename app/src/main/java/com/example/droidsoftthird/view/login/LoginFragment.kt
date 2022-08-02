377b9b25d0ae70083234bd0f682c22e143c9e274
tsemb012 <tsukasa0171015@gmail.com>
2022-08-03 01:16:17 +0900

    DST-96 add: composable method & preview

--- app/src/main/java/com/example/droidsoftthird/view/login/LoginFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/view/login/LoginFragment.kt
@@ -6,16 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return ComposeView(requireContext()).apply {
-            setContent { Text(text = "Hello JetpackCompose") }
-
+        return ComposeView(requireContext()).apply { 
+            setContent { MessageCard("JetpackCompose") }
         }
     }
+
+    @Composable
+    private fun MessageCard(s: String) {
+        Text(text = "Hello $s")
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(s = "JetPackCompose")
+    }//パラメータを受け取らないコンポーズ可能な関数であること
 }
