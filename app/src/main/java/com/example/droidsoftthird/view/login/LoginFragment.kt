3f5d9f965d294762516c902e7f946680d417ddb2
tsemb012 <tsukasa0171015@gmail.com>
2022-08-03 01:04:09 +0900

    DST-97 update: gradle for JetpackCompose

---
+++ app/src/main/java/com/example/droidsoftthird/view/login/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.example.droidsoftthird.view.login
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setContent { Text(text = "Hello JetpackCompose") }
+
+        }
+    }
+}
