1247a18ff1156f3dea34cdd553f201b0f8956b64
tsemb012 <tsukasa0171015@gmail.com>
2022-08-13 03:04:59 +0900

    DST-87 add: Strings

---
+++ app/src/main/java/com/example/droidsoftthird/ui/entrance/EmailState.kt
@@ -0,0 +1,21 @@
+package com.example.droidsoftthird.ui.entrance
+
+import java.util.regex.Pattern
+
+//TODO API側のValidationと齟齬がないか確認する。
+private const val EMAIL_VALIDATION_REGEX = "^(.+)@(.+)\$"
+
+class EmailState : TextFieldState(validator = ::isEmailValid, errorFor = ::emailValidationError)
+
+/**
+ * Returns an error to be displayed or null if no error was found
+ */
+private fun emailValidationError(email: String): String {
+    return "Invalid email: $email"
+}
+
+private fun isEmailValid(email: String): Boolean {
+    return Pattern.matches(EMAIL_VALIDATION_REGEX, email)
+}
+
+val EmailStateSaver = textFieldStateSaver(EmailState())
