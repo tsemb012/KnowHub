5907b7f347e0d2c0546d1e84014723074ff7d060
tsemb012 <tsukasa0171015@gmail.com>
2022-08-13 23:54:07 +0900

    DST-87 add: Dummy Repository

---
+++ app/src/main/java/com/example/droidsoftthird/repository/UserRepository.kt
@@ -0,0 +1,37 @@
+package com.example.droidsoftthird.repository
+
+import androidx.compose.runtime.Immutable
+
+
+sealed class TestUser {
+    @Immutable
+    data class LoggedInUser(val email: String) : TestUser()
+    object GuestUser : TestUser()
+    object NoUserLoggedIn : TestUser()
+}
+//TODO 実験的に追加するRepository　RailsApiRepositoryと統合する。
+object UserRepository {
+
+    private var _user:TestUser = TestUser.NoUserLoggedIn
+    val user: TestUser
+        get() = _user
+
+    @Suppress("UNUSED_PARAMETER")
+    fun signIn(email: String, password: String) {
+        _user = TestUser.LoggedInUser(email)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun signUp(email: String, password: String) {
+        _user = TestUser.LoggedInUser(email)
+    }
+
+    fun signInAsGuest() {
+        _user = TestUser.GuestUser
+    }
+
+    fun isKnownUserEmail(email: String): Boolean {
+        // if the email contains "sign up" we consider it unknown
+        return !email.contains("signup")
+    }
+}
