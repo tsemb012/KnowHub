790f9c6f380a18052b17fa3c855d3dcdf43d1acd
tsemb012 <tsukasa0171015@gmail.com>
2022-08-13 23:51:00 +0900

    DST-87 add: State for composable

--- app/src/main/java/com/example/droidsoftthird/ui/entrance/state/TextFieldState.kt
+++ app/src/main/java/com/example/droidsoftthird/ui/entrance/state/TextFieldState.kt
@@ -1,4 +1,4 @@
-package com.example.droidsoftthird.ui.entrance
+package com.example.droidsoftthird.ui.entrance.state
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
