91a68f87ceffc725f4cc1d8ac3ba6f00c7f70d0c
tsemb012 <tsukasa0171015@gmail.com>
2022-09-17 12:52:04 +0900

    DST-182 update: params

--- app/src/main/java/com/example/droidsoftthird/repository/RailsApiRepository.kt
+++ app/src/main/java/com/example/droidsoftthird/repository/RailsApiRepository.kt
@@ -3,11 +3,12 @@ package com.example.droidsoftthird.repository
 import com.example.droidsoftthird.model.User
 import com.example.droidsoftthird.model.fire_model.Group
 import com.example.droidsoftthird.model.json.SignUpJson
+import com.example.droidsoftthird.model.rails_model.ApiGroup
 
 interface RailsApiRepository {
 
     suspend fun postNewUser(singup: SignUpJson): User?
 
-    suspend fun createGroup(group: Group): String?
+    suspend fun createGroup(group: ApiGroup): String?
 
 }
