807a1d12c888b8dbb2f7d2e7dd61773b1f53c547
tsemb012 <tsukasa0171015@gmail.com>
2022-09-17 12:53:00 +0900

    DST-182 fix: Group

--- app/src/main/java/com/example/droidsoftthird/model/fire_model/Group.kt
+++ app/src/main/java/com/example/droidsoftthird/model/fire_model/Group.kt
@@ -1,40 +1,31 @@
 package com.example.droidsoftthird.model.fire_model
 
-import com.example.droidsoftthird.model.request.PostGroup
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
 
-data class Group (
-        val hostUserId: String,
-        val storageRef: String,
-        val groupName: String,
-        val groupIntroduction: String,
-        val groupType: String,
-        val prefecture: String,
-        val city: String,
-        val facilityEnvironment: String,
-        val basis: String,
-        val frequency:Int,
-        val minAge:Int,
-        val maxAge:Int,
-        val minNumberPerson:Int,
-        val maxNumberPerson :Int,
-        val isChecked:Boolean,
-) {
-    fun toJson() =
-        PostGroup.Request(
-                hostId = hostUserId,
-                name = groupName,
-                introduction = groupIntroduction,
-                type = groupType,
-                prefecture = prefecture,
-                city = city,
-                facilityEnvironment = facilityEnvironment,
-                frequencyBasis = basis,
-                frequencyTimes = frequency,
-                maxAge = maxAge,
-                minAge = minAge,
-                maxNumber = maxNumberPerson,
-                minNumber = maxNumberPerson,
-                isSameSexuality = isChecked,
-                imageUrl = storageRef,
-        )
-}
+data class Group (//FireStoreで使用する場合は、Nullableかつ初期値にNullを入れる必要がある。
+        val hostUserId: String? = null,
+        val storageRef: String? = null,
+        val groupName: String? = null,
+        val groupIntroduction: String? = null,
+        val groupType: String? = null,
+        val prefecture: String? = null,
+        val city: String? = null,
+        val facilityEnvironment: String? = null,
+        val basis: String? = null,
+        val frequency:Int? = null,
+        val minAge:Int? = null,
+        val maxAge:Int? = null,
+        val minNumberPerson:Int? = null,
+        val maxNumberPerson :Int? = null,
+        @field:JvmField
+        val isChecked:Boolean? = null,
+        val members:List<String>?=null,
+        @DocumentId
+        val groupId: String? = null,
+        @ServerTimestamp
+        val timeStamp: Date? = null
+        //TODO DateAPI　から　Date &TimeAPIに変換する。
+        //TODO RawGroupからGroupに変換する使用に変更する。GroupのPOJO見直しする際に。
+)
