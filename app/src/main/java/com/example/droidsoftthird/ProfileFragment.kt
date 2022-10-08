84f1155e17aaf3fadb2d993cfc4fc18ef0862bc5
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-10-08 19:35:20 +0900

    DST-310 update: draft of ProfileFragment

--- app/src/main/java/com/example/droidsoftthird/ProfileFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ProfileFragment.kt
@@ -1,6 +1,12 @@
 package com.example.droidsoftthird
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.droidsoftthird.databinding.FragmentProfileBinding
 import com.wada811.databinding.dataBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,5 +15,26 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProfileFragment:Fragment(R.layout.fragment_profile) {
 
     private val binding: FragmentProfileBinding by dataBinding()
+    private val viewModel:ProfileViewModel by viewModels()
+    private val navController by lazy { NavHostFragment.findNavController(this) }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+        setupNavAppBar()
+    }
+
+    private fun setupNavAppBar() {
+        //val navController = (requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        //TODO 上記のnavControllerで遷移できない場合は、あらためて検討する。
+        val appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupWithNavController(
+                binding.collapsingToolbarLayout,
+                binding.materialToolbar.apply { title = " " },
+                navController,
+                appBarConfiguration
+        )
+    }
 }
