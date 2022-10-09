5149a24554bf0e516ecf6dd512c446c499bba6c4
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-10-09 14:19:51 +0900

    DST-310 add: UserDetail & PostUserDetail.kt

--- app/src/main/java/com/example/droidsoftthird/model/rails_model/UserDetail.kt
+++ app/src/main/java/com/example/droidsoftthird/model/rails_model/UserDetail.kt
@@ -1,7 +1,27 @@
 package com.example.droidsoftthird.model.rails_model
 
+import com.example.droidsoftthird.model.request.PostUserDetail
+
 data class UserDetail (
         val userId: String,
-
-
-)
+        val userName: String,
+        val userImage: String,
+        val backGroundImage: String,
+        val comment:String,
+        val gender: String,
+        val age: Int,
+        val area: Area,
+        val groups: List<ApiGroup>,
+) {
+    fun toJson() =
+        PostUserDetail(
+                userId = userId,
+                userName = userName,
+                userImage = userImage,
+                backGroundImage = backGroundImage,
+                comment = comment,
+                gender = gender,
+                age = age,
+                area = area,
+        )
+}
--- app/src/main/java/com/example/droidsoftthird/model/response/GetUserDetail.kt
+++ app/src/main/java/com/example/droidsoftthird/model/response/GetUserDetail.kt
@@ -1,21 +1,35 @@
 package com.example.droidsoftthird.model.response
 
 import com.example.droidsoftthird.model.rails_model.Area
+import com.example.droidsoftthird.model.rails_model.UserDetail
 import com.squareup.moshi.Json
 
 data class GetUserDetail (
-        @Json(name = "id")
-        val userId: String,
-        @Json(name = "user_name")
-        val userName: String? = null,
-        @Json(name = "user_image")
-        val userImage: String,
-        @Json(name = "back_ground_image")
-        val backGroundImage: String,
-        val comment:String,
-        val gender: String,
-        val age: Int,
-        val area: Area,
-        val groups: List<GetGroup>,
-        //val schedules: List<GetShcedule> TODO スケジュール実装後に追加
-)
+    @Json(name = "id")
+    val userId: String,
+    @Json(name = "user_name")
+    val userName: String? = null,
+    @Json(name = "user_image")
+    val userImage: String,
+    @Json(name = "back_ground_image")
+    val backGroundImage: String,
+    val comment:String,
+    val gender: String,
+    val age: Int,
+    val area: Area,
+    val groups: List<GetGroup>,
+    //val schedules: List<GetShcedule> TODO スケジュール実装後に追加
+) {
+    fun toEntity() =
+        UserDetail(
+                userId = userId,
+                userName = userName,
+                userImage = userImage,
+                backGroundImage = backGroundImage,
+                comment = comment,
+                gender = gender,
+                age = age,
+                area = area,
+                groups = groups.map { it.toEntity() },
+        )
+}
