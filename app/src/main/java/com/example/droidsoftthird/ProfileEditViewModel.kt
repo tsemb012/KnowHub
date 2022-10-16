96f75c287567e83e0d06d67b60f9f70a3db4c58c
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-10-16 12:45:04 +0900

    DST-310 update: ProfileEditViewModel.kt

--- app/src/main/java/com/example/droidsoftthird/ProfileEditViewModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ProfileEditViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.droidsoftthird
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,42 +13,33 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileEditViewModel @Inject constructor(private val UseCase: ProfileUseCase): ViewModel( ) {
+class ProfileEditViewModel @Inject constructor(private val useCase: ProfileUseCase): ViewModel( ) {
 
-    private val _loadState by lazy { MutableLiveData<LoadState>() }
+    private val _loadState by lazy { MutableLiveData<LoadState>() }//TODO LoadStateを改造し、Messageを内包させ、Ui側でProgressとToastで表現する。
     private val _rawUserDetail by lazy { MutableLiveData<UserDetail>() }
-    private val _editedUserDetail by lazy { MutableLiveData<UserDetail>() }
-    private val _name = MutableLiveData<String>()
-    private val _comment = MutableLiveData<String>()
-    val _userImage: MutableLiveData<String> = MutableLiveData()
-    val _backgroundImage: MutableLiveData<String> = MutableLiveData()
-    private val _age: MutableLiveData<Int> by lazy { MutableLiveData() }
-    private val _area: MutableLiveData<Area> by lazy { MutableLiveData() }//TODO validationをどうつけるか検討する。
-    private val _gender: MutableLiveData<String> by lazy { MutableLiveData() }
+    private val _editedUserDetail by lazy {
+        MediatorLiveData<UserDetail>().also { result ->
+            result.addSource(userName) { result.value = result.value?.copy(userName = it) }
+            result.addSource(comment) { result.value = result.value?.copy(comment = it) }
+            result.addSource(userImage) { result.value = result.value?.copy(userImage = it) }
+            result.addSource(backgroundImage) { result.value = result.value?.copy(backgroundImage = it) }
+        }
+    }
 
     val uiModel by lazy {
         combine(
                 ProfileUiModel(),
                 _rawUserDetail,
-                _name,
-                _comment,
-                _userImage,
-                _backgroundImage,
-                _age,
-                _area,
-                _gender
-        ) { current, _name, _comment, _userImage, _backgroundImage, _rawUserDetail, _age, _area, _gender ->
-            ProfileUiModel(current, _name, _comment, _userImage, _backgroundImage, _rawUserDetail, _age, _area, _gender)
+                _editedUserDetail,
+                _loadState,
+        ) { current, _rawUserDetail, _editedUserDetail, _loadState ->
+            ProfileUiModel(current, _rawUserDetail, _editedUserDetail, _loadState)
         }
     }
 
-    fun postAge(age: Int) { _age.value = age }
-    fun postArea(area: Area) { _area.value = area }
-    fun postGender(gender: UserDetail.Gender) { _gender.value = gender.name.lowercase() }
-
     fun fetchUserDetail() {
         val job = viewModelScope.launch {
-            runCatching { UseCase.fetchUserDetail() }
+            runCatching { useCase.fetchUserDetail() }
                 .onSuccess {
                     _rawUserDetail.value = it
                     _loadState.value = LoadState.Loaded(it)
@@ -58,11 +50,23 @@ class ProfileEditViewModel @Inject constructor(private val UseCase: ProfileUseCa
         job.start()
     }
 
-    fun submitProfile( TODO("userDetail") ) {
-        profileUseCase.
+    fun submitProfile() {
+        val job = viewModelScope.launch {
+            val userDetail = _editedUserDetail.value ?: return@launch
+            runCatching { useCase.updateUserDetail(userDetail) }
+                .onSuccess { _loadState.value = LoadState.Loaded(it) }
+                .onFailure { _loadState.value = LoadState.Error(it) }
+        }
+        _loadState.value = LoadState.Loading(job)
+        job.start()
     }
 
-    //TODO 画面遷移を作成。
-    //TODO userDetailを作成。
+    val userName: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    val comment: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    val userImage: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    val backgroundImage: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    fun postAge(age: Int) { _editedUserDetail.value = _editedUserDetail.value?.copy(age = age) }
+    fun postArea(area: Area) { _editedUserDetail.value = _editedUserDetail.value?.copy(area = area) }
+    fun postGender(gender: UserDetail.Gender) { _editedUserDetail.value = _editedUserDetail.value?.copy(gender = gender.name.lowercase()) }
 
 }
