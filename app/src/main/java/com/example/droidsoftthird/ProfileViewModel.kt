3fd44c5efa9fc5f349514bab87da4754987c3d37
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-10-22 11:07:05 +0900

    DST-391 fix: hilt of ProfileViewModel.kt

--- app/src/main/java/com/example/droidsoftthird/ProfileViewModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ProfileViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.droidsoftthird.model.domain_model.UserDetail
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-
+@HiltViewModel
 class ProfileViewModel @Inject constructor(private val useCase: ProfileUseCase): ViewModel() {
 
     //TODO 基本的に個人情報はRailsAPIにしまう。ユーザー名とユーザー画像だけはFirebaseとRailsAPIどちらにも入れる。
