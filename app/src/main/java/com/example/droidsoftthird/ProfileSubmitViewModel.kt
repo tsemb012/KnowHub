de8dd363a955735264c08eee77b61d98eab91c3a
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-10-29 16:01:13 +0900

    DST-391 fix: ProfileSubmit Classes

--- app/src/main/java/com/example/droidsoftthird/ProfileSubmitViewModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ProfileSubmitViewModel.kt
@@ -1,42 +1,45 @@
 package com.example.droidsoftthird
 
 import android.net.Uri
+import androidx.core.net.toUri
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.droidsoftthird.model.domain_model.Area
 import com.example.droidsoftthird.model.domain_model.UserDetail
+import com.example.droidsoftthird.model.domain_model.initializedUserDetail
 import com.example.droidsoftthird.model.fire_model.LoadState
 import com.example.droidsoftthird.utils.combine
 import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.storage.StorageReference
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 open class ProfileSubmitViewModel (private val useCase: ProfileUseCase): ViewModel() {
 
-    protected val rawUserDetail by lazy { MutableLiveData<UserDetail>() }
-    private val _editedUserDetail by lazy {
-        MediatorLiveData<UserDetail>().also { result ->
-            result.addSource(userName) { result.value = result.value?.copy(userName = it) }
-            result.addSource(comment) { result.value = result.value?.copy(comment = it) }
-        }
-    }
-    protected val temporalUserImage by lazy { MutableLiveData<Uri>() }
-    protected val temporalBackgroundImage by lazy { MutableLiveData<Uri>() }
-    protected val loadState by lazy { MutableLiveData<LoadState>() }//TODO LoadStateを改造し、Messageを内包させ、Ui側でProgressとToastで表現する。
-
+    protected val rawUserDetail by lazy { MutableLiveData(initializedUserDetail) }
+    private val _editedUserDetail by lazy { MutableLiveData(initializedUserDetail) }
+    protected val temporalUserImage by lazy { MutableLiveData("".toUri()) }
+    protected val temporalBackgroundImage by lazy { MutableLiveData("".toUri()) }
+    private val isTextFilled by lazy { MediatorLiveData<Boolean>().apply {
+        addSource(bindingUserName) { value = it.isNullOrBlank() }
+        addSource(bindingComment) { value = it.isNullOrBlank() }
+    } }
+    protected val loadState by lazy { MutableLiveData<LoadState>(LoadState.Initialized) }//TODO LoadStateを改造し、Messageを内包させ、Ui側でProgressとToastで表現する。
 
     val uiModel by lazy {
         combine(
-            ProfileUiModel(),
-            rawUserDetail,
-            _editedUserDetail,
-            temporalUserImage,
-            temporalBackgroundImage,
-            loadState,
-        ) { current, _rawUserDetail, _editedUserDetail, _temporalUserImage, _temporalBackgroundImage, _loadState ->
-            ProfileUiModel(current, _rawUserDetail, _editedUserDetail, _temporalUserImage, _temporalBackgroundImage, _loadState)
+                ProfileUiModel(),
+                rawUserDetail,
+                _editedUserDetail,
+                temporalUserImage,
+                temporalBackgroundImage,
+                isTextFilled,
+                loadState,
+        ) { current, _rawUserDetail, _editedUserDetail, _temporalUserImage, _temporalBackgroundImage, isFilledText, _loadState ->
+            ProfileUiModel(current, _rawUserDetail, _editedUserDetail, _temporalUserImage, _temporalBackgroundImage, isFilledText, _loadState)
         }
     }
 
@@ -51,7 +54,7 @@ open class ProfileSubmitViewModel (private val useCase: ProfileUseCase): ViewMod
                     val userImagePath = async { temporalUserImage.value?.let { useCase.uploadImage(it) } }
                     val backgroundImagePath = async { temporalBackgroundImage.value?.let { useCase.uploadImage(it) } }
                     useCase.updateAuthProfile(authUserDetail)//TODO ここなんか変かも
-                    val userDetail = _editedUserDetail.value?.copy(userImage = userImagePath.await().toString(), backgroundImage = backgroundImagePath.await().toString())
+                    val userDetail = buildUserDetail(userImagePath, backgroundImagePath)
                     userDetail?.let {
                         when (type) {
                             SubmitType.CREATE -> useCase.createUserDetail(it)
@@ -66,10 +69,20 @@ open class ProfileSubmitViewModel (private val useCase: ProfileUseCase): ViewMod
         job.start()
     }
 
+    private suspend fun buildUserDetail(
+            userImagePath: Deferred<Result<StorageReference>?>,
+            backgroundImagePath: Deferred<Result<StorageReference>?>,
+    ) = uiModel.value?.editedUserDetail?.copy(
+            userImage = userImagePath.await().toString(),
+            backgroundImage = backgroundImagePath.await().toString(),
+            userName = bindingUserName.value ?: throw Exception("userName is null"),
+            comment = bindingComment.value ?: throw Exception("comment is null"),
+    )
+
     enum class SubmitType { EDIT, CREATE }
 
-    val userName: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
-    val comment: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    val bindingUserName: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
+    val bindingComment: MutableLiveData<String> by lazy { MutableLiveData<String>("") }
     fun postAge(age: Int) { _editedUserDetail.value = _editedUserDetail.value?.copy(age = age) }
     fun postArea(area: Area) { _editedUserDetail.value = _editedUserDetail.value?.copy(area = area) }
     fun postGender(gender: UserDetail.Gender) { _editedUserDetail.value = _editedUserDetail.value?.copy(gender = gender.name.lowercase()) }
--- app/src/main/java/com/example/droidsoftthird/ProfileUiModel.kt
+++ app/src/main/java/com/example/droidsoftthird/ProfileUiModel.kt
@@ -11,10 +11,16 @@ data class ProfileUiModel (
         val isSubmitEnabled: Boolean = false,
         val temporalUserImage: Uri? = null,
         val temporalBackgroundImage: Uri? = null,
+
         val loadState: LoadState = LoadState.Initialized,
 ) {
-    val age = editedUserDetail.age.toString()
-    val area = editedUserDetail.area.let { it?.prefecture?.name + ", " + it?.city?.name }
+    val gender = editedUserDetail.gender.let { it.ifBlank { NO_SETTING } }
+    val age = editedUserDetail.age.toString().let { if(it == "-1") NO_SETTING else  it }
+    val area = editedUserDetail.area.let {
+        if (it == null)  NO_SETTING
+        else if (it.city == null) it.prefecture.name
+        else it?.prefecture?.name + ", " + it?.city?.name
+    }
 
     companion object {
         operator fun invoke(
@@ -23,21 +29,24 @@ data class ProfileUiModel (
                 _editedUserDetail: UserDetail,
                 _temporalUserImage: Uri,
                 _temporalBackgroundImage: Uri,
+                isTextFilled: Boolean,
                 _loadState: LoadState,
         ) = ProfileUiModel(
                 rawUserDetail = _rawUserDetail,
                 editedUserDetail = _editedUserDetail,
-                isSubmitEnabled = isValid(current),
+                isSubmitEnabled = isValid(current, isTextFilled),
                 temporalUserImage = _temporalUserImage,
                 temporalBackgroundImage = _temporalBackgroundImage,
                 loadState = _loadState,
         )
 
-        private fun isValid(current: ProfileUiModel) =
+        private const val NO_SETTING = "未設定"
+
+        private fun isValid(current: ProfileUiModel, isTextFilled: Boolean) =
                 isChangedUserDetail(current) &&
                 isStoredTemporalImages(current) &&
-                isNotEmptyUserDetail(current)
-
+                isNotEmptyUserDetail(current) &&
+                isTextFilled
 
         private fun isChangedUserDetail(current: ProfileUiModel) =
                 current.rawUserDetail != current.editedUserDetail
@@ -45,10 +54,7 @@ data class ProfileUiModel (
                 current.temporalUserImage != null
                 && current.temporalBackgroundImage != null
         private fun isNotEmptyUserDetail(current: ProfileUiModel) =
-                current.editedUserDetail.userName.isNotBlank()
-                && current.editedUserDetail.comment.isNotBlank()
-                && current.editedUserDetail.gender.isNotBlank()
-                && current.editedUserDetail.comment.isNotEmpty()
+                   current.editedUserDetail.gender.isNotBlank()
                 && current.editedUserDetail.area != null
                 && current.editedUserDetail.age != -1
         }
