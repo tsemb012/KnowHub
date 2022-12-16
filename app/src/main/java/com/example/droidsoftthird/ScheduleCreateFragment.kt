4a971d16e9a1e99861ace757250b4e422118b214
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-12-16 23:09:07 +0900

    DST-459 add: google map

--- app/src/main/java/com/example/droidsoftthird/ScheduleCreateFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleCreateFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.callbacks.onCancel
 import com.afollestad.materialdialogs.datetime.timePicker
@@ -101,7 +102,10 @@ class ScheduleCreateFragment:Fragment(R.layout.fragment_schedule_create) {
             }
 
             includeScheduleCreateLocation.itemScheduleCreate.setOnClickListener {
-                childFragmentManager.let { LocationDialogFragment().show(it, "location") }
+
+                //findNavController().navigate()
+
+                //childFragmentManager.let { LocationDialogFragment().show(it, "location") }
             }
             /*includeScheduleCreateGroup.itemScheduleCreate.setOnClickListener()*/
         }
