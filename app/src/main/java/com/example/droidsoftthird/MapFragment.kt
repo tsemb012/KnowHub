11276100c2652df43b3ddd169196a067f9b09bdd
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2022-12-16 23:09:07 +0900

    DST-459 add: google map

---
+++ app/src/main/java/com/example/droidsoftthird/MapFragment.kt
@@ -0,0 +1,38 @@
+package com.example.droidsoftthird
+
+import androidx.fragment.app.Fragment
+
+class MapFragment: Fragment() {
+
+/*    private lateinit var binding: FragmentMapBinding
+    private val viewModel: MapViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_map, container, false
+        )
+
+        binding.mapViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }*/
+}
