08e9925dbc89e41db3f309f123598ed0c8def21f
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2023-01-15 22:16:49 +0900

    DST-459 文字列から位置情報を取得できるようにUseCaseを追加

---
+++ app/src/main/java/com/example/droidsoftthird/MapUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.droidsoftthird
+
+import com.example.droidsoftthird.model.domain_model.Place
+import com.example.droidsoftthird.model.domain_model.ViewPort
+import com.example.droidsoftthird.repository.BaseRepositoryImpl
+import javax.inject.Inject
+
+class MapUseCase  @Inject constructor(private val repository: BaseRepositoryImpl) {
+    suspend fun searchPlaces(query: String, viewPort: ViewPort): List<Place> = repository.searchPlaces(query, viewPort)
+    //suspend fun fetchPOIs(type: String): List<Marker> = repository.fetchPOIs
+    //suspend fun fetchCurrentLocation():  TODO 現在地を取得するようにする。
+    //suspend fun fetchPlaceDetail: TODO 場所の詳細を取得する　→　PlaceDetailのデータクラスを取得するようにする。
+}
