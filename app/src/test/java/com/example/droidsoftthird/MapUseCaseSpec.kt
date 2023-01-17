58b60f58917c25bb0a3a6a953f8517bd7d21b52f
tsemb012 <65854024+tsemb012@users.noreply.github.com>
2023-01-17 21:22:01 +0900

    DST-459 gradle周りのtestを少し綺麗に。テスト描きかけ（単体テストと結合テストもう少し理解してから）

---
+++ app/src/test/java/com/example/droidsoftthird/MapUseCaseSpec.kt
@@ -0,0 +1,37 @@
+package com.example.droidsoftthird
+
+import com.example.droidsoftthird.model.domain_model.ViewPort
+import com.example.droidsoftthird.repository.BaseRepositoryImpl
+import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.test.TestCase
+import org.junit.Rule
+import javax.inject.Inject
+
+
+@HiltAndroidTest
+class MapUseCaseSpec: DescribeSpec(){
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+    @Inject
+    lateinit var repository: BaseRepositoryImpl
+    val viewPort = ViewPort(northEast = LatLng(35.659281, 139.746433), southWest = LatLng(35.658581, 139.745433))
+
+    override fun beforeTest(f: suspend (TestCase) -> Unit) {
+        //super.beforeTest(f)
+        hiltRule.inject()
+    }
+
+    init {
+        describe("MapUseCase") {
+            context("searchPlaces") {
+                it("should return a list of places") {
+                    repository.searchPlaces("ラーメン", viewPort)
+
+                }
+            }
+        }
+    }
+}
