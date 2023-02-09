4ea5b924f765f6d160951ca6ff660fc412116cc7
tsemb012 <tsukasa0171015@gmail.com>
2023-02-09 20:22:14 +0900

    DST-459 Composeの階層を浅くするように修正

--- app/src/main/java/com/example/droidsoftthird/composable/ChipGroup.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/ChipGroup.kt
@@ -6,14 +6,15 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class) //TODO 実験的なAPIなので気をつける。
 @Composable
-fun ChipGroup(chips: List<String>, onClickChip: (String) -> Unit) {
+fun ChipGroup(chips: MutableState<List<String>>, onClickChip: (String) -> Unit) {
     Row {
-        chips.forEach { chip ->
+        chips.value.forEach { chip ->
             AssistChip(
                 label = { Text(chip) },
                 onClick = { onClickChip(chip) },
