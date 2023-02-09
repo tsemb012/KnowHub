64a90a6cf656169a395806f03f0aeb9c5fb4967a
tsemb012 <tsukasa0171015@gmail.com>
2023-02-09 20:22:14 +0900

    DST-459 Composeの階層を浅くするように修正

--- app/src/main/java/com/example/droidsoftthird/MapFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/MapFragment.kt
@@ -4,26 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.droidsoftthird.composable.ChipGroup
+import com.example.droidsoftthird.composable.CircleProgressBar
 import com.example.droidsoftthird.composable.DropDown
 import com.example.droidsoftthird.composable.SearchBox
-import com.example.droidsoftthird.composable.map.GoogleMapView
+import com.example.droidsoftthird.model.domain_model.Place
+import com.example.droidsoftthird.model.presentation_model.LoadState
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 import dagger.hilt.android.AndroidEntryPoint
 
 //DONE 図にまとめるOK　→　マップ入力画面UIの情報収集を行うOK　→　UIを確定させるOK　→　データのやりとりをざっくり検討するOK　→　TODOを設置して全体像を掴む　→　
@@ -44,6 +41,17 @@ import dagger.hilt.android.AndroidEntryPoint
 
 //TODO ネットがつながったらComposableのpreviewを用意するようにする。
 
+/*TODO 設計方針
+*   ViewModelの中に値を保存
+*   Composeの中にViewModelは渡さず、Fragmentでここの値に分割する。
+*   ベースのComposeに対して、外部からComposeを入れていくように
+*   Composeを入れる際は、mutableの値 と callbackを渡してあげる。
+*   rememberは使わないくても良いの？　→　rememberはRecomposeで内部の変数が消えてしまうのを防いでいるが、データベースからくる値は全てViewModelに入っているので、
+*   Fragment側から渡す値に関しては、不要。
+*   uiStateはComposeから出さない。LoadStateはViewModel内で分解し、Stateでラップするように
+*   2つComposeをまたぐ場合は？　→　この場合は上部に新しいComposewo作りラップするようにする。
+*/
+
 @AndroidEntryPoint
 class MapFragment: Fragment() {
 
@@ -59,37 +67,103 @@ class MapFragment: Fragment() {
 
         return ComposeView(requireContext()).apply {
             setContent {
-                var isMapLoaded by remember { mutableStateOf(false) }
                 val cameraPositionState = rememberCameraPositionState { position = defaultCameraPosition }
-                GoogleMapView(
-                        modifier = Modifier,
-                        cameraPositionState = cameraPositionState,
-                        onMapLoaded = { isMapLoaded = true },
-                        updateCameraPosition = { northEast, southWest -> viewModel.updateViewPoint(northEast, southWest) },
-                        placesLoadState = viewModel.placesLoadState,
-                        placeDetailLoadState = viewModel.placeDetailLoadState,
-                        currentPoint = viewModel.centerPoint,
-                        currentRadius = viewModel.radius,
-                        onMarkerClick = { placeId -> viewModel.fetchPlaceDetail(placeId) },
-                        composableSearchBox = { SearchBox(viewModel.query) { viewModel.searchByText() } },
-                        composableDropDown = { DropDown(viewModel.selections, viewModel.selectedType) },
-                        composableChipGroup = { ChipGroup(viewModel.selections.value) { viewModel.searchByPoi() } },
-                )
-                if (!isMapLoaded) {
-                    AnimatedVisibility(
-                        modifier = Modifier.fillMaxSize(),
-                        visible = !isMapLoaded,
-                        enter = EnterTransition.None,
-                        exit = fadeOut()
-                    ) {
-                        CircularProgressIndicator(
-                            modifier = Modifier
-                                .background(MaterialTheme.colors.background)
-                                .wrapContentSize()
-                        )
+                var isMapLoaded by remember { mutableStateOf(false) }
+                val uiSettings by remember { mutableStateOf(MapUiSettings(compassEnabled = false)) }
+                val mapProperties by remember { mutableStateOf(MapProperties(mapType = MapType.NORMAL)) }
+                val mapVisible by remember { mutableStateOf(true) }
+                val isLoading: Boolean = viewModel.placesLoadState.value is LoadState.Loading || (viewModel.placeDetailLoadState.value is LoadState.Loading)
+                val isError: Boolean = viewModel.placesLoadState.value is LoadState.Error || (viewModel.placeDetailLoadState.value is LoadState.Error)
+
+                com.example.droidsoftthird.composable.BottomModal(
+                    placeDetailLoadState = viewModel.placeDetailLoadState,
+                    onConfirm = ::confirmPlace
+                ) {
+                    Column {
+                        if (isLoading) LinearProgressIndicator(Modifier.fillMaxWidth())
+                        Box {
+                            if (mapVisible) {
+                                GoogleMap(
+                                    cameraPositionState = cameraPositionState,
+                                    modifier = Modifier,
+                                    properties = mapProperties,
+                                    uiSettings = uiSettings,
+                                    onMapLoaded = { isMapLoaded = true },
+                                    onPOIClick = { },
+                                    content = function(cameraPositionState, viewModel.centerPoint, viewModel.radius),
+                                )
+                                Column {
+                                    Row(modifier = Modifier.height(56.dp).padding(top = 16.dp)) {
+                                        SearchBox(viewModel.query) { viewModel.searchByText() }
+                                        DropDown(viewModel.selections, viewModel.selectedType)
+                                    }
+                                    ChipGroup(viewModel.selections) { viewModel.searchByPoi() }
+                                }
+                            }
+                        }
                     }
                 }
+                CircleProgressBar(isMapLoaded)
             }
         }
+    }
+
+    fun confirmPlace () {
+       // val place = viewModel.places.value.find { it.id == place.id }
+        //TODO Fragment間で値の受け渡しをするように
+    }
+
+
+
+    @Composable
+    private fun function(
+        cameraPositionState: CameraPositionState,
+        centerPoint: MutableState<LatLng>,
+        radius: MutableState<Int>,
+
+
+    ): @Composable () -> Unit =
+        {
+            val updateCameraPosition: (northEast: LatLng, southWest: LatLng) -> Unit = { _, _ -> }
+            if (!cameraPositionState.isMoving) {//カメラの動きが止まった時のデータをViewModelにあげるようにする。
+                cameraPositionState.projection?.visibleRegion?.latLngBounds?.let {
+                    updateCameraPosition(it.northeast, it.southwest)
+                    cameraPositionState
+                    centerPoint.value = it.center
+                    radius.value = distanceInMeters(
+                            it.center.latitude,
+                            it.center.longitude,
+                            it.center.latitude,
+                            it.northeast.longitude
+                    ).toInt()
+                }
+            }
+
+            if (viewModel.placesLoadState.value is LoadState.Loaded<*> ) {
+                viewModel.placesLoadState.value.getValueOrNull<List<Place>>()?.forEach {
+                    Marker(
+                            state = MarkerState(position = LatLng(it.location.lat, it.location.lng)),
+                            tag = it.placeId,
+                            title = it.name,
+                            snippet = it.types[0],
+                            onClick = { marker ->
+                                viewModel.fetchPlaceDetail((marker.tag.toString()))
+                                true
+                            }
+                    )
+                }
+                //viewModel.placeDetailLoadState.value = LoadState.Modified(Pair(viewModel.placeDetailLoadState.value.getValueOrNull() , null))
+            }
+        }
+
+    private fun distanceInMeters(lat1: Double, lng1: Double, lat2: Double, lng2: Double): Double {
+        val earthRadius = 6371.0 // 地球の半径
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLng = Math.toRadians(lng2 - lng1)
+        val a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+            Math.sin(dLng / 2) * Math.sin(dLng / 2)
+        val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+        return (earthRadius * c * 1000.0) * 0.8
     }
 }
--- app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
@@ -1,27 +1,49 @@
 package com.example.droidsoftthird.composable
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.droidsoftthird.model.domain_model.PlaceDetail
+import com.example.droidsoftthird.model.presentation_model.LoadState
 import kotlinx.coroutines.launch
 
 @Composable
 @OptIn(ExperimentalMaterialApi::class)
 fun BottomModal(
-    bottomSheetState: ModalBottomSheetState,
+    placeDetailLoadState: MutableState<LoadState>,
+    onConfirm: () -> Unit = {},
     content: @Composable () -> Unit = {}
 ) {
-    //val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+    val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
+    var name by remember { mutableStateOf("") }
+    var address by remember { mutableStateOf("") }
+
     //TODO 内部に入力情報を保持するように　→　修正されたら内部で修正するようにする。　→　修正されたら
     ModalBottomSheetLayout(
         sheetState = bottomSheetState,
         sheetContent = {
+
+            when (placeDetailLoadState.value) {
+                is LoadState.Error -> {
+                    Toast.makeText(LocalContext.current, placeDetailLoadState.value.getErrorOrNull().toString(), Toast.LENGTH_SHORT).show()
+                    placeDetailLoadState.value = LoadState.Initialized
+                }
+                is LoadState.Loaded<*> -> {
+                    placeDetailLoadState.value.getValueOrNull<PlaceDetail>()?.let {
+                        scope.launch { bottomSheetState.show() }
+                        name = it.name
+                        address = it.formattedAddress ?: ""
+                    }
+                    placeDetailLoadState.value = LoadState.Initialized
+                }
+            }
             Column {
-                TextField(value = "", onValueChange = {})
-                TextField(value = "", onValueChange = {})
+                TextField(value = name, onValueChange = {name = it})
+                TextField(value = address, onValueChange = {address = it})
                 TextField(value = "", onValueChange = {})
                 TextField(value = "", onValueChange = {})
                 Row {
@@ -30,6 +52,7 @@ fun BottomModal(
                     }
                     Button(onClick = { scope.launch { bottomSheetState.show() } }) {
                         Text(text = "Show")
+                        onConfirm()
                     }
                 }
             }
