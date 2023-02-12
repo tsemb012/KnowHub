12061f0fbc54381cb7a98b29f7393de09dd371eb
tsemb012 <tsukasa0171015@gmail.com>
2023-02-12 23:26:53 +0900

    DST-459 下部モーダルをリファクタ

--- app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
@@ -1,12 +1,14 @@
 package com.example.droidsoftthird.composable
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.droidsoftthird.model.domain_model.PlaceDetail
+import androidx.compose.ui.unit.dp
+import com.example.droidsoftthird.R
+import com.example.droidsoftthird.model.domain_model.*
 import com.example.droidsoftthird.model.presentation_model.LoadState
 import kotlinx.coroutines.launch
 
@@ -14,45 +16,35 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterialApi::class)
 fun BottomModal(
     placeDetailLoadState: MutableState<LoadState>,
-    onConfirm: () -> Unit = {},
+    onConfirm: (EditedPlaceDetail?) -> Unit = {},
     content: @Composable () -> Unit = {}
 ) {
     val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     val scope = rememberCoroutineScope()
-    var name by remember { mutableStateOf("") }
-    var address by remember { mutableStateOf("") }
+    var editedPlaceDetail by remember { mutableStateOf<EditedPlaceDetail?>(null) }
 
-    //TODO 内部に入力情報を保持するように　→　修正されたら内部で修正するようにする。　→　修正されたら
     ModalBottomSheetLayout(
         sheetState = bottomSheetState,
+        //modifier = Modifier.background(editedPlaceDetail?.color.let { if (it != null) Color(android.graphics.Color.parseColor(it)) else Color.White  }),
         sheetContent = {
-
-            when (placeDetailLoadState.value) {
-                is LoadState.Error -> {
-                    Toast.makeText(LocalContext.current, placeDetailLoadState.value.getErrorOrNull().toString(), Toast.LENGTH_SHORT).show()
-                    placeDetailLoadState.value = LoadState.Initialized
+            if (placeDetailLoadState.value is LoadState.Loaded<*>) {
+                placeDetailLoadState.value.getValueOrNull<PlaceDetail>()?.let {
+                    scope.launch { bottomSheetState.show() }
+                    editedPlaceDetail = it.toEditedPlaceDetail()
                 }
-                is LoadState.Loaded<*> -> {
-                    placeDetailLoadState.value.getValueOrNull<PlaceDetail>()?.let {
-                        scope.launch { bottomSheetState.show() }
-                        name = it.name
-                        address = it.formattedAddress ?: ""
-                    }
-                    placeDetailLoadState.value = LoadState.Initialized
-                }
+                placeDetailLoadState.value = LoadState.Initialized
             }
             Column {
-                TextField(value = name, onValueChange = {name = it})
-                TextField(value = address, onValueChange = {address = it})
-                TextField(value = "", onValueChange = {})
-                TextField(value = "", onValueChange = {})
+                ListItem(label = stringResource(R.string.map_place_name), value = editedPlaceDetail?.name ?: "")
+                ListItem(label = stringResource(R.string.map_address), value = editedPlaceDetail?.formattedAddress ?: "")
+                ListItem(label = stringResource(R.string.map_category), value = editedPlaceDetail?.type ?: "")
+                EditableListItem(label = stringResource(R.string.map_memo), value = editedPlaceDetail?.memo?: "", onTextChanged = { editedPlaceDetail = editedPlaceDetail?.copy(memo = it) })
                 Row {
                     Button(onClick = { scope.launch { bottomSheetState.hide() } }) {
-                        Text(text = "Close")
+                        Text(text = stringResource(R.string.cancel))
                     }
-                    Button(onClick = { scope.launch { bottomSheetState.show() } }) {
-                        Text(text = "Show")
-                        onConfirm()
+                    Button(onClick = { onConfirm(editedPlaceDetail) }) {
+                        Text(text = stringResource(id = R.string.general_confirm))
                     }
                 }
             }
@@ -61,8 +53,36 @@ fun BottomModal(
     )
 }
 
+@Composable
+fun ListItem (label: String, value: String) {
+    Row {
+        Text(text = label, modifier = Modifier
+            .height(60.dp)
+            .width(100.dp))
+        Text(text = value, modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth())
+    }
+}
+
+@Composable
+fun EditableListItem (label: String, value: String, onTextChanged: (String) -> Unit) {
+    Row {
+        Text(text = label, modifier = Modifier
+            .height(60.dp)
+            .width(100.dp))
+        TextField(value = value, modifier = Modifier.fillMaxWidth(), onValueChange = { onTextChanged(it) })
+    }
+}
+
 @Preview
 @Composable
 fun BottomModalPreview() {
     //BottomModal(isExpanded = true)
+}
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    ListItem(label = "Name", value = "value")
 }
