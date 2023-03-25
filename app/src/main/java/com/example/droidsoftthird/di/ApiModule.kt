0414e9d2d8e20cf694f7a4e9f80bb8792a39f0da
tsemb012 <tsukasa0171015@gmail.com>
2023-03-25 18:10:37 +0900

    DST-471 EventにビデオチャットのIdを追加するように

--- app/src/main/java/com/example/droidsoftthird/di/ApiModule.kt
+++ app/src/main/java/com/example/droidsoftthird/di/ApiModule.kt
@@ -33,9 +33,9 @@ class ApiModule {
 
     companion object {
         //private const val BASE_URL = "http://10.0.2.2:3000/" //エミュレーターで起動する場合
-        //private const val BASE_URL = "http://192.168.10.104:3000/"
+        private const val BASE_URL = "http://192.168.10.104:3000/"
         //private const val BASE_URL = "http://192.168.200.21:3000/"
-        private const val BASE_URL = "http://192.168.200.68:3000/"
+        //private const val BASE_URL = "http://192.168.200.68:3000/"
         private const val TOKEN_ID_KEY = "token_id_key"
     }
 
--- app/src/main/java/com/example/droidsoftthird/model/domain_model/EventDetail.kt
+++ app/src/main/java/com/example/droidsoftthird/model/domain_model/EventDetail.kt
@@ -6,6 +6,7 @@ import java.time.LocalTime
 data class EventDetail (
         val eventId: String,
         val hostId: String,
+        val roomId: String, //TODO オンラインでもオフラインでも部屋番号は生成できるように。いつでもオンラインに変更できるように。
         val name: String,
         val comment: String,
         val date: LocalDate,
