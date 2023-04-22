6ea39975852fdc0f7ea7df9290b0f17e66365f45
tsemb012 <tsukasa0171015@gmail.com>
2023-04-22 15:40:15 +0900

    DST-261 マイページでも画像を取得できるように

--- app/src/main/java/com/example/droidsoftthird/usecase/GroupUseCase.kt
+++ app/src/main/java/com/example/droidsoftthird/usecase/GroupUseCase.kt
@@ -11,15 +11,13 @@ class GroupUseCase @Inject constructor(private val repository: BaseRepositoryImp
 
     suspend fun createGroup(group: EditedGroup) = repository.createGroup(group)
     suspend fun fetchGroups(areaCode: Int? = null, areaCategory: String? = null) = repository.fetchGroups(areaCode, areaCategory).map {
-        it.map { group ->
-            group.copy(
-                storageRef = fetchGroupImage(group.storageRef) //TODO 将来的にS3に置き換えるか、サーバーサイドでURLを取得するようにする
-            )
-        }
+        it.map { group -> group.copy(storageRef = fetchGroupImage(group.storageRef)) } //TODO 将来的にS3に置き換えるか、サーバーサイドでURLを取得するようにする
     }
     suspend fun fetchGroupDetail(groupId: String) = repository.fetchGroupDetail(groupId)
     suspend fun userJoinGroup(groupId: String) = repository.userJoinGroup(groupId)
-    suspend fun fetchJoinedGroups() = repository.fetchJoinedGroups()
+    suspend fun fetchJoinedGroups() = repository.fetchJoinedGroups().map { group ->
+        group.copy(storageRef = fetchGroupImage(group.storageRef))
+    }
     suspend fun uploadPhoto(value: Uri) = repository.uploadPhoto(value)
     suspend fun fetchCountByArea() = repository.fetchGroupCountByArea()
     private suspend fun fetchGroupImage(imagePath: String) = repository.fetchStorageImage(imagePath)
