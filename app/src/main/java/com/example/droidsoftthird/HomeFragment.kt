49522c9190ce38e5a1bfad26046805af5fc38df7
tsemb012 <tsukasa0171015@gmail.com>
2023-04-30 18:33:45 +0900

    DST-530 リファクタリング

--- app/src/main/java/com/example/droidsoftthird/HomeFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/HomeFragment.kt
@@ -1,45 +1,35 @@
 package com.example.droidsoftthird
 
 import android.annotation.SuppressLint
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.NavDirections
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import com.example.droidsoftthird.databinding.FragmentHomeBinding
 import com.example.droidsoftthird.ui.entrance.Screen
 import com.example.droidsoftthird.ui.entrance.navigate
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding;
-    private lateinit var navController: NavController
-    private val viewModel:HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //-----Enable Menu
-        setHasOptionsMenu(true);
+        setHasOptionsMenu(true)
     }
 
     override fun onResume() {
         super.onResume()
         if (FirebaseAuth.getInstance().currentUser == null) {
             backToWelcome()
-            /* TODO ホーム画面でユーザーのログイン状況をチェックして、ログインしていなければログイン画面に遷移させる
-            clearCache()
-            signOut()*/
         }
     }
 
@@ -47,7 +37,7 @@ class HomeFragment: Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -57,51 +47,30 @@ class HomeFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        val navController = navHostFragment.navController
 
-        //-----ViewPager Objects
+        // ViewPager Objects
         val homeViewPagerAdapter = HomeViewPagerAdapter(this)
-        val viewPager = binding.pager
-        viewPager.adapter = homeViewPagerAdapter
-        viewPager.isUserInputEnabled = false
+        binding.pager.apply {
+            adapter = homeViewPagerAdapter
+            isUserInputEnabled = false
+        }
 
-        //-----TabLayout&ViewPager Linking
-        val tabLayout: TabLayout = binding.tabLayout
-        TabLayoutMediator(tabLayout, viewPager
-        ) { tab: TabLayout.Tab, position: Int -> tab.text = "OBJECT" + (position + 1) }.attach()
+        // TabLayout & ViewPager Linking
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position -> tab.text = "OBJECT${position + 1}" }.attach()
 
-        //-----SettingTab *After Linking Tab&ViewPager*
-        requireNotNull(tabLayout.getTabAt(0)).setText(R.string.recommendation)
-        requireNotNull(tabLayout.getTabAt(1)).setText(R.string.map)
+        // Setting Tab
+        binding.tabLayout.getTabAt(0)?.setText(R.string.recommendation)
+        binding.tabLayout.getTabAt(1)?.setText(R.string.map)
 
-        //-----Navigation to AddGroupFragment by FloatingActionButton
-
-        binding.floatingActionButton.setOnClickListener(View.OnClickListener { v ->
-            val action: NavDirections =
-                HomeFragmentDirections.actionHomeFragmentToAddGroupFragment()
-            Navigation.findNavController(v).navigate(action)
-        })//TODO 関心の分離のため、Eventクラスを用いてクリックイベントをViewModelに移行する。
-    }
-
-    private fun backToWelcome() { navigate(Screen.Welcome, Screen.Home) }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.home,menu)
-        val primaryWhite = "#F6FFFE"
-        menu.getItem(0).icon?.apply {
-            mutate() // Drawableを変更可能にする
-            setColorFilter(android.graphics.Color.parseColor(primaryWhite), PorterDuff.Mode.SRC_ATOP) // アイコンを白くする
+        // Navigation to AddGroupFragment by FloatingActionButton
+        binding.floatingActionButton.setOnClickListener { v ->
+            val action = HomeFragmentDirections.actionHomeFragmentToAddGroupFragment()
+            navController.navigate(action)
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.getItemId()) {
-            R.id.filter -> {
-                TODO("Filterの機能を追加する。")
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
+    private fun backToWelcome() {
+        navigate(Screen.Welcome, Screen.Home)
     }
 }
