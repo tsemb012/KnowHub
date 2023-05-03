8f5e98605bd8a22227abfed3cd8f21f363152d10
tsemb012 <tsukasa0171015@gmail.com>
2023-05-03 15:40:58 +0900

    DST-473 グループタイプをタイプに置き換えるように

--- app/src/main/java/com/example/droidsoftthird/model/domain_model/ApiGroup.kt
+++ app/src/main/java/com/example/droidsoftthird/model/domain_model/ApiGroup.kt
@@ -1,30 +1,31 @@
 package com.example.droidsoftthird.model.domain_model
 
+import com.example.droidsoftthird.R
 import com.example.droidsoftthird.model.infra_model.json.request.PostGroupJson
 
-data class EditedGroup (
-        val groupId: String?,
-        val hostUserId: String,
-        val storageRef: String,
-        val groupName: String,
-        val groupIntroduction: String,
-        val groupType: String,
-        val prefecture_code: Int,
-        val city_code: Int,
-        val facilityEnvironment: String,
-        val basis: String,
-        val frequency:Int,
-        val minAge:Int,
-        val maxAge:Int,
-        val maxNumberPerson :Int,
-        val isChecked:Boolean,
+data class EditedGroup(
+    val groupId: String?,
+    val hostUserId: String,
+    val storageRef: String,
+    val groupName: String,
+    val groupIntroduction: String,
+    val groupType: GroupType,
+    val prefecture_code: Int,
+    val city_code: Int,
+    val facilityEnvironment: String,
+    val basis: String,
+    val frequency:Int,
+    val minAge:Int,
+    val maxAge:Int,
+    val maxNumberPerson:Int,
+    val isChecked:Boolean,
 ) {
         fun toJson() =
                 PostGroupJson(
                         hostId = hostUserId,
                         name = groupName,
                         introduction = groupIntroduction,
-                        groupType = groupType,
+                        groupType = groupType.name.lowercase(),
                         prefecture_code = prefecture_code,
                         city_code = city_code,
                         facilityEnvironment = facilityEnvironment,
@@ -55,3 +56,21 @@ data class ApiGroup (//TODO ApiGroupからGroupに名前を変更する//Firebas
     val maxNumberPerson :Int,
     val isChecked:Boolean,
 )
+
+enum class GroupType(val displayNameId : Int) {
+    NONE(R.string.no_set),
+    SEMINAR(R.string.seminar),
+    WORKSHOP(R.string.workshop),
+    MOKUMOKU(R.string.mokumoku),
+    OTHER(R.string.other)
+}
+
+enum class FacilityEnvironment {
+    LIBRARY,
+    CAFE_RESTAURANT,
+    CO_WORKING_SPACE,
+    PAID_STUDY_SPACE,
+    PARK,
+    ONLINE,
+    OTHER
+}
