ec482d87fbefd147e9b6f81bb87d03cdab218308
tsemb012 <tsukasa0171015@gmail.com>
2023-05-03 20:18:47 +0900

    DST-473 グループの表示を修正

--- app/src/main/java/com/example/droidsoftthird/dialogs/GroupTypeDialogFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/dialogs/GroupTypeDialogFragment.kt
@@ -14,15 +14,13 @@ class GroupTypeDialogFragment: DialogFragment() {
     private val viewModel: GroupAddViewModel by viewModels({requireParentFragment()})
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
-        val items = GroupType.values().map { getString(it.displayNameId) }.toTypedArray()
+        val items = GroupType.toArrayForDisplay().map { getString(it.displayNameId) }.toTypedArray()
         activity?.let {
             val builder = AlertDialog.Builder(it)
             builder
                 .setTitle(R.string.group_name)
                 .setIcon(R.drawable.ic_baseline_group_24)
-                .setItems(items) { _, which ->
-                    viewModel.postGroupType(GroupType.values()[which])
-                }
+                .setItems(items) { _, which -> viewModel.postGroupType(GroupType.values()[which]) }
             return    builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
