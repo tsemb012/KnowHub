86c2dbd65938b792a677a8fbd0400c6a5c53e0d6
tsemb012 <tsukasa0171015@gmail.com>
2023-06-11 22:31:39 +0900

    DST-575 0件の時の表示

--- app/src/main/java/com/example/droidsoftthird/composable/group/content/PagingGroupList.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/group/content/PagingGroupList.kt
@@ -5,16 +5,31 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import com.example.droidsoftthird.R
+import com.example.droidsoftthird.composable.shared.EmptyMessage
 import com.example.droidsoftthird.model.domain_model.ApiGroup
+import com.example.droidsoftthird.model.domain_model.FacilityEnvironment
+import com.example.droidsoftthird.model.domain_model.FrequencyBasis
+import com.example.droidsoftthird.model.domain_model.GroupType
+import kotlinx.coroutines.flow.flowOf
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -28,10 +43,37 @@ fun PagingGroupList(groups: LazyPagingItems<ApiGroup>, navigate: (String) -> Uni
             modifier = Modifier.fillMaxWidth(),
             contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp)
         ) {
+            if (groups.itemCount == 0) item { EmptyMessage (R.string.no_groups_found) }
             items(groups) {
                 it?.let { group -> GroupListItem(group, navigate) }
             }
         }
         PullRefreshIndicator(refreshing, state, Modifier.align(Alignment.TopCenter))
     }
+}
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true, showSystemUi = true)
+@Composable
+fun GroupListEmptyPreview() {
+    val group1 = ApiGroup(
+        groupId = "1", hostUserId = "1", groupName = "group1", groupIntroduction = "group1",
+        groupType = GroupType.OTHER_GROUP_TYPE, prefecture = "prefecture", city = "city", isOnline = false,
+        facilityEnvironment = FacilityEnvironment.OTHER_FACILITY_ENVIRONMENT, basis = FrequencyBasis.MONTHLY,
+        frequency = 1, minAge = 10, maxAge = 40, maxNumberPerson = 10, isChecked = false, storageRef = "https://images.pexels.com/photos/9954174/pexels-photo-9954174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+    val group2 = ApiGroup(
+        groupId = "2", hostUserId = "2", groupName = "group2", groupIntroduction = "group2",
+        groupType = GroupType.OTHER_GROUP_TYPE, prefecture = "prefecture", city = "city", isOnline = false,
+        facilityEnvironment = FacilityEnvironment.OTHER_FACILITY_ENVIRONMENT, basis = FrequencyBasis.MONTHLY,
+        frequency = 1, minAge = 10, maxAge = 40, maxNumberPerson = 10, isChecked = false, storageRef = "https://images.pexels.com/photos/17079798/pexels-photo-17079798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+    val group3 = ApiGroup(
+        groupId = "3", hostUserId = "3", groupName = "group3", groupIntroduction = "group3",
+        groupType = GroupType.OTHER_GROUP_TYPE, prefecture = "prefecture", city = "city", isOnline = false,
+        facilityEnvironment = FacilityEnvironment.OTHER_FACILITY_ENVIRONMENT, basis = FrequencyBasis.MONTHLY,
+        frequency = 1, minAge = 10, maxAge = 40, maxNumberPerson = 10, isChecked = false, storageRef = "https://images.pexels.com/photos/14880025/pexels-photo-14880025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+    val pagingData = PagingData.from(listOf(group1, group2, group3))
+    val collectAsLazyPagingItems = flowOf(pagingData).collectAsLazyPagingItems()
+    Surface(color = Color.White) {
+        PagingGroupList(groups = collectAsLazyPagingItems as LazyPagingItems<ApiGroup>, navigate = {})
+    }
+
 }
