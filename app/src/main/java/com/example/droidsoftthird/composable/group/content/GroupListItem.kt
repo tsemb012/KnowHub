33c117e0099d02b776f152cdbd80ea98d296f6a8
tsemb012 <tsukasa0171015@gmail.com>
2023-06-18 00:42:58 +0900

    DST-575 色の修正

--- app/src/main/java/com/example/droidsoftthird/composable/group/content/GroupListItem.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/group/content/GroupListItem.kt
@@ -1,22 +1,25 @@
 package com.example.droidsoftthird.composable.group.content
 
-import android.graphics.drawable.Icon
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.Comment
 import androidx.compose.material.icons.filled.Group
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -25,6 +28,8 @@ import com.example.droidsoftthird.model.domain_model.FacilityEnvironment
 import com.example.droidsoftthird.model.domain_model.FrequencyBasis
 import com.example.droidsoftthird.model.domain_model.GroupType
 import com.example.droidsoftthird.model.domain_model.Style
+import com.example.droidsoftthird.R
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -33,9 +38,11 @@ fun GroupListItem(group: ApiGroup, navigateToDetail: (String) -> Unit) {
         shape = RoundedCornerShape(24.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        elevation = 8.dp,
-        onClick = { group.groupId?.let { navigateToDetail(it) } }
+            .padding(vertical = 4.dp),
+        elevation = 4.dp,
+        onClick = { group.groupId?.let { navigateToDetail(it) } },
+        backgroundColor = colorResource(id = R.color.base_100),
+        //border = BorderStroke(1.dp, Color.Black.copy(alpha = 0.1F))
     ) {
         Row(
             modifier = Modifier
@@ -54,30 +61,33 @@ fun GroupListItem(group: ApiGroup, navigateToDetail: (String) -> Unit) {
             )
             Spacer(modifier = Modifier.width(16.dp))
             //TODO 下記をまとめて再利用できるように
+
             Column {
-                /*Text(text = group.groupName, style = MaterialTheme.typography.h6, color = Color.Gray)
+                Text(text = group.groupName, style = MaterialTheme.typography.h6, color = Color.DarkGray, maxLines = 1)
 
                 val itemList = listOf(
-                    Icons.Filled.Group to group.groupIntroduction,
-                    "facilityEnvironment" to group.facilityEnvironment,
-                    "style" to group.style,
-                    "basis" to group.basis,
-                    "frequency" to group.frequency,
-                    "minAge" to group.minAge,
-                    "maxAge" to group.maxAge,
-                    "maxNumberPerson" to group.maxNumberPerson,
-                    "isChecked" to group.isChecked,
+                    Icons.Filled.Group to stringResource(id = R.string.availability, group.availability),
+                    Icons.Filled.LocationOn to group.location,
+                    Icons.Filled.Comment to group.groupIntroduction,
                 )
 
-                Column(modifier = Modifier.padding(start = 8.dp, top = 4.dp, bottom = 4.dp)) {
+                Column(modifier = Modifier.padding(start = 16.dp, top = 4.dp, bottom = 4.dp)) {
 
-                    Text(text = group.groupIntroduction, style = MaterialTheme.typography.body1)
-                    Text(
-                        text = "${group.prefecture} ${group.city}",
-                        style = MaterialTheme.typography.caption,
-                        modifier = Modifier.align(Alignment.End)
-                    )
-                }*/
+                    itemList.forEach { (icon, text) ->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            androidx.compose.material.Icon(
+                                imageVector = icon,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(16.dp)
+                                    .align(Alignment.CenterVertically),
+                                tint = Color.Gray
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = text, style = MaterialTheme.typography.body1, color = Color.DarkGray, maxLines = 2)
+                        }
+                    }
+                }
             }
         }
     }
