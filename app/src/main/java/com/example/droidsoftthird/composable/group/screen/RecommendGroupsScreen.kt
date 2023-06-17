1d85fe69a5cf841818a949c472d52bd91dee3c0d
tsemb012 <tsukasa0171015@gmail.com>
2023-06-17 21:18:07 +0900

    DST-575 リファクタ

--- app/src/main/java/com/example/droidsoftthird/composable/group/screen/RecommendGroupsScreen.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/group/screen/RecommendGroupsScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
