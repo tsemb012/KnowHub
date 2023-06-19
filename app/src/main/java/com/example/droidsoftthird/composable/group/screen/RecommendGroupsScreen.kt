2b976bce66552336888860b7f031afeb3b590301
tsemb012 <tsukasa0171015@gmail.com>
2023-06-20 00:17:52 +0900

    DST-575 BottomDialogの全面修正とリファクタリング

--- app/src/main/java/com/example/droidsoftthird/composable/group/screen/RecommendGroupsScreen.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/group/screen/RecommendGroupsScreen.kt
@@ -1,36 +1,38 @@
 package com.example.droidsoftthird.composable.group.screen
 
-import androidx.compose.animation.*
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.example.droidsoftthird.R
 import com.example.droidsoftthird.RecommendGroupsViewModel
 import com.example.droidsoftthird.composable.group.content.PagingGroupList
-import com.example.droidsoftthird.model.domain_model.*
-import com.example.droidsoftthird.model.domain_model.ApiGroup.FilterCondition.Companion.getPrefectureCode
-import com.example.droidsoftthird.repository.csvloader.CityCsvLoader
-import com.example.droidsoftthird.repository.csvloader.PrefectureCsvLoader
+import com.example.droidsoftthird.composable.group.content.bottomdialog.AnimatedFilterConditionDialog
+import com.example.droidsoftthird.model.domain_model.ApiGroup
 
 enum class FilterContentDestination { HOME, PREFECTURE, CITY }
 
