997e14b7a94e4166a4b9f7f2221d02999d8dd872
tsemb012 <tsukasa0171015@gmail.com>
2023-07-04 23:53:43 +0900

    DST-582 タイトルを調整

--- app/src/main/java/com/example/droidsoftthird/composable/group/content/GroupList.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/group/content/GroupList.kt
@@ -7,25 +7,20 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.droidsoftthird.R
+import com.example.droidsoftthird.composable.shared.BoldTitleItem
 import com.example.droidsoftthird.composable.shared.CommonLinearProgressIndicator
 import com.example.droidsoftthird.composable.shared.EmptyMessage
 import com.example.droidsoftthird.model.domain_model.ApiGroup
 import com.example.droidsoftthird.model.presentation_model.LoadState
 
 @Composable
-fun GroupList(groupsLoadState: LoadState, navigate: (String) -> Unit) {
+fun MyPageGroupList(groupsLoadState: LoadState, navigate: (String) -> Unit) {
 
     val groups = groupsLoadState.getValueOrNull<List<ApiGroup>>()
     val isLoading = groupsLoadState is LoadState.Loading
@@ -38,7 +33,7 @@ fun GroupList(groupsLoadState: LoadState, navigate: (String) -> Unit) {
                 contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp)
             ) {
 
-                item { Text(text = "マイページ", color = Color.DarkGray, style = MaterialTheme.typography.h4, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 8.dp)) }
+                item { BoldTitleItem("マイページ", Modifier.padding(bottom = 8.dp)) }
                 if (!isLoading && groups?.isEmpty() == true) item { EmptyMessage(R.string.join_or_create_group) }
                 groups?.let { list ->
                     items(list.size) { GroupListItem(groups[it], navigate) }
@@ -52,3 +47,4 @@ fun GroupList(groupsLoadState: LoadState, navigate: (String) -> Unit) {
     }
     error?.let { Toast.makeText(LocalContext.current, it.message, Toast.LENGTH_SHORT).show() }
 }
+
