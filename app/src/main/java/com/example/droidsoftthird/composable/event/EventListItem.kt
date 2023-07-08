aa109edf4e144c8eb32a844e1687564b6c97db32
tsemb012 <tsukasa0171015@gmail.com>
2023-07-08 16:08:03 +0900

    DST-582 ざっくりStatusを加えてみる。

--- app/src/main/java/com/example/droidsoftthird/composable/event/EventListItem.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/event/EventListItem.kt
@@ -1,12 +1,17 @@
 package com.example.droidsoftthird.composable.event
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AvTimer
 import androidx.compose.material.icons.filled.Group
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.droidsoftthird.composable.ItemDescription
+import com.example.droidsoftthird.R
+import com.example.droidsoftthird.composable.shared.ItemDescription
 import com.example.droidsoftthird.composable.shared.FundamentalCard
+import com.example.droidsoftthird.composable.shared.ItemStatus
 import com.example.droidsoftthird.model.domain_model.ItemEvent
 
 @Composable
@@ -19,10 +24,25 @@ fun EventListItem(event: ItemEvent, navigateToDetail: (String) -> Unit) {
 
 @Composable
 fun EventCardContent(event: ItemEvent) {
-    val itemList = listOf(
+
+    val descriptionList = listOf(
         Triple(Icons.Filled.Group, event.groupName ?: "", 1),
         Triple(Icons.Filled.AvTimer, event.period.toString(), 1),
         Triple(Icons.Filled.LocationOn, event.placeName ?: "", 1),
-        )
-    ItemDescription(event.name, itemList)
+    )
+
+    /*val statusColor = when {
+        event.period
+    }*/
+
+    val statusList = listOf(
+        Triple(Icons.Filled.Group, R.color.gph_dark_red, "テスト"),
+        Triple(Icons.Filled.AvTimer, R.color.gray, "テスト"),
+    )
+
+    Column {
+        ItemDescription(event.name, descriptionList)
+        ItemStatus(statusList)
+        Text(text = "人数")
+    }
 }
--- app/src/main/java/com/example/droidsoftthird/composable/shared/ItemDescription.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/shared/ItemDescription.kt
@@ -1,4 +1,4 @@
-package com.example.droidsoftthird.composable
+package com.example.droidsoftthird.composable.shared
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
