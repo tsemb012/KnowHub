efecf2c2bb303a55fc15682d97b656e451d98505
tsemb012 <tsukasa0171015@gmail.com>
2023-07-10 22:45:11 +0900

    DST-582 イベント追加ボタンをセット

--- app/src/main/java/com/example/droidsoftthird/ScheduleHomeFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ScheduleHomeFragment.kt
@@ -1,38 +1,21 @@
 package com.example.droidsoftthird
 
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.viewModels
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.droidsoftthird.composable.group.content.CommonAddButton
 import com.example.droidsoftthird.databinding.FragmentScheduleHomeBinding
-import com.google.android.material.tabs.TabLayoutMediator
 import com.wada811.databinding.dataBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint //このActivityはHiltが使うと宣言し、依存関係をHiltから引っ張ってくる。
+@AndroidEntryPoint
 class ScheduleHomeFragment: Fragment(R.layout.fragment_schedule_home) {
 
     private val viewModel: ScheduleCalendarViewModel by activityViewModels() //TODO 生存期間流すぎるので後で短くするように
@@ -54,14 +37,13 @@ class ScheduleHomeFragment: Fragment(R.layout.fragment_schedule_home) {
                 pager.adapter = ScheduleHomePagerAdapter(this@ScheduleHomeFragment)
                 pager.isUserInputEnabled = false
             }
-            /*tabLayout.let {
-                TabLayoutMediator(it, pager) { tab, position -> tab.text = "OBJECT" + (position + 1) }.attach()
-                it.getTabAt(0)?.setText(R.string.schedule_calendar) ?: throw IllegalStateException()
-                it.getTabAt(1)?.setText(R.string.schedule_list) ?: throw IllegalStateException()
-            }*/
-            floatingActionButton.setOnClickListener {
-                val action = ScheduleHomeFragmentDirections.actionScheduleHomeFragmentToScheduleCreateFragment()
-                Navigation.findNavController(it).navigate(action)
+
+            floatingActionButtonCompose.setContent {
+                CommonAddButton(
+                    label = "イベントを追加",
+                    navigate = { findNavController().navigate(ScheduleHomeFragmentDirections.actionScheduleHomeFragmentToScheduleCreateFragment()) },
+                    modifier = Modifier.padding(bottom = 32.dp, end = 16.dp)
+                )
             }
         }
     }
