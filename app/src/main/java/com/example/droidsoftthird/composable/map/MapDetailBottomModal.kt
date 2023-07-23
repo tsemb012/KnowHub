776b93c1677f0d6fdd49b7f8c68830139ba8ccca
tsemb012 <tsukasa0171015@gmail.com>
2023-07-23 19:55:11 +0900

    DST-584 戻るボタンのセット

--- app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/map/MapDetailBottomModal.kt
@@ -2,7 +2,6 @@ package com.example.droidsoftthird.composable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Language
@@ -10,12 +9,7 @@ import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidsoftthird.PlaceMapViewState
--- app/src/main/java/com/example/droidsoftthird/composable/map/place/PlaceMapScreen.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/map/place/PlaceMapScreen.kt
@@ -1,15 +1,33 @@
 package com.example.droidsoftthird.composable.map.place
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.example.droidsoftthird.PlaceMapViewModel
+import com.example.droidsoftthird.R
 import com.example.droidsoftthird.composable.PlaceMapBottomModal
 import com.example.droidsoftthird.composable.shared.CommonLinearProgressIndicator
+import com.example.droidsoftthird.composable.shared.SharedBackButton
 import com.example.droidsoftthird.model.domain_model.EditedPlace
 
 @Composable
-fun PlaceMapScreen(viewModel: PlaceMapViewModel, confirmPlace: (EditedPlace?) -> Unit) {
+fun PlaceMapScreen(
+    viewModel: PlaceMapViewModel,
+    confirmPlace: (EditedPlace?) -> Unit,
+    navigateUp: () -> Unit
+) {
     val viewState = viewModel.viewState
     val updateViewState = viewModel::updateViewState
     val fetchPlaceDetail = viewModel::fetchPlaceDetail
@@ -24,9 +42,12 @@ fun PlaceMapScreen(viewModel: PlaceMapViewModel, confirmPlace: (EditedPlace?) ->
         updateViewState = updateViewState,
         onConfirm = { confirmPlace(it) }
     ) {
+
         GoogleMapScreen(viewState, updateViewState, fetchPlaceDetail) { _isMapLoading.value = false }
-        PlaceSearchComponent(searchByText, searchByCategory)
+        Column(modifier = Modifier.padding(4.dp)) {
+            SharedBackButton(navigateUp)
+            PlaceSearchComponent(searchByText, searchByCategory)
+        }
         if (viewState.value.isLoading || isMapLoading) CommonLinearProgressIndicator()
     }
 }
-
