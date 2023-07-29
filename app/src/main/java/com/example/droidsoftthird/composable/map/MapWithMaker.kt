0cad6f94e60bdbcfb8b9c8288ac54d6ada9865a5
tsemb012 <tsukasa0171015@gmail.com>
2023-07-29 10:59:04 +0900

    DST-583 地図に枠線を設置する

--- app/src/main/java/com/example/droidsoftthird/composable/map/MapWithMaker.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/map/MapWithMaker.kt
@@ -1,10 +1,15 @@
 package com.example.droidsoftthird.composable.map
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.droidsoftthird.model.domain_model.EventDetail
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -17,22 +22,33 @@ fun MapWithMarker(event: MutableState<EventDetail?>, modifier: Modifier = Modifi
     if (place != null) {
         val eventPosition = CameraPosition.fromLatLngZoom(LatLng(place.location.lat, place.location.lng), 15f)
         val cameraPositionState = rememberCameraPositionState{ position = eventPosition }
-        GoogleMap(
-            cameraPositionState = cameraPositionState,
-            modifier = modifier.clip(RoundedCornerShape(10)),
-            properties = MapProperties(mapType = MapType.NORMAL),
-            uiSettings = MapUiSettings(
-                compassEnabled = false,
-                scrollGesturesEnabled = false,
-                zoomGesturesEnabled = false,
-            ),
+
+        Box(
+            modifier = modifier
+                .clip(RoundedCornerShape(10))
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray,
+                    shape = RoundedCornerShape(10)
+                )
         ) {
-            Marker(
-                state = MarkerState(position = LatLng(place.location.lat, place.location.lng)),
-                tag = place.placeId,
-                title = place.name,
-                snippet = place.category,
-            )
+            GoogleMap(
+                cameraPositionState = cameraPositionState,
+                modifier = Modifier.fillMaxSize(),
+                properties = MapProperties(mapType = MapType.NORMAL),
+                uiSettings = MapUiSettings(
+                    compassEnabled = false,
+                    scrollGesturesEnabled = false,
+                    zoomGesturesEnabled = false,
+                ),
+            ) {
+                Marker(
+                    state = MarkerState(position = LatLng(place.location.lat, place.location.lng)),
+                    tag = place.placeId,
+                    title = place.name,
+                    snippet = place.category,
+                )
+            }
         }
     }
 }
