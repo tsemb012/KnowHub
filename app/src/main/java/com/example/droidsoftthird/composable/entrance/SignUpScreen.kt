cb673c282bed1742d7a77f9064dde72a359e5087
tsemb012 <tsukasa0171015@gmail.com>
2023-08-06 08:37:16 +0900

    DST-746 ロゴ修正

--- app/src/main/java/com/example/droidsoftthird/composable/entrance/SignUpScreen.kt
+++ app/src/main/java/com/example/droidsoftthird/composable/entrance/SignUpScreen.kt
@@ -79,14 +79,6 @@ fun SignUpContent(
         val confirmationPasswordFocusRequest = remember { FocusRequester() }
         val emailState = remember { EmailState() }
 
-        Image(
-            painter = painterResource(id = R.drawable.full_logo_transparent_no_buffer), // ここに画像リソースを指定
-            contentDescription = null, // アクセシビリティのための説明文
-            alignment = Alignment.TopCenter, // 画像を中央に配置
-            modifier = Modifier.fillMaxWidth().height(200.dp), // 画像のサイズを指定
-            contentScale = ContentScale.Fit // 必要に応じて画像をクロップまたは他の方法で調整
-        )
-
         BrandLogo()
 
         Spacer(modifier = Modifier.height(32.dp))
