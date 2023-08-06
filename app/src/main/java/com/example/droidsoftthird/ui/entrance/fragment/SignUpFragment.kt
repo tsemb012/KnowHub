38a39defe97b0f7ea57b5c6834c73841d65163e6
tsemb012 <tsukasa0171015@gmail.com>
2023-08-06 10:40:34 +0900

    DST-746 インストラクションページを作成

--- app/src/main/java/com/example/droidsoftthird/ui/entrance/fragment/SignUpFragment.kt
+++ app/src/main/java/com/example/droidsoftthird/ui/entrance/fragment/SignUpFragment.kt
@@ -9,37 +9,38 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.droidsoftthird.R
 import com.example.droidsoftthird.composable.entrance.InstructionPage
-import com.example.droidsoftthird.composable.entrance.InstructionPage1
 import com.example.droidsoftthird.ui.entrance.Screen
 import com.example.droidsoftthird.composable.entrance.SignUpScreen
 import com.example.droidsoftthird.composable.entrance.SignUpEvent
+import com.example.droidsoftthird.composable.shared.SharedConfirmButton
 import com.example.droidsoftthird.ui.entrance.navigate
 import com.example.droidsoftthird.vm.entrance.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class SignUpFragment: Fragment() {
@@ -72,7 +73,9 @@ class SignUpFragment: Fragment() {
             setContent {
                 MaterialTheme {
 
-                    Box( modifier = Modifier.background(colorResource(id = R.color.base_100)).fillMaxSize())  {
+                    Box( modifier = Modifier
+                        .background(colorResource(id = R.color.base_100))
+                        .fillMaxSize())  {
                         val pagerState = rememberPagerState { 6 }
                         HorizontalPager(state = pagerState) { page ->
                             when (page) {
@@ -102,16 +105,32 @@ class SignUpFragment: Fragment() {
                                 )
                             }
                         }
-                        Row(horizontalArrangement = Arrangement.Center,  modifier = Modifier
-                            .align(Alignment.BottomCenter)
-                            .fillMaxWidth()) {
-                            repeat(5) { page ->
-                                Spacer(modifier = Modifier.width(4.dp))
-                                Surface(
-                                    modifier = Modifier.size(10.dp),
-                                    shape = CircleShape,
-                                    color = if (page == pagerState.currentPage) Color.Gray else Color.LightGray
-                                ) {}
+
+                        Column (modifier = Modifier.align(Alignment.BottomCenter)) {
+                            Divider()
+                            Spacer(modifier = Modifier.height(12.dp))
+                            Row(
+                                horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()
+                            ) {
+                                repeat(6) { page ->
+                                    Spacer(modifier = Modifier.width(4.dp))
+                                    Surface(
+                                        modifier = Modifier.size(10.dp),
+                                        shape = CircleShape,
+                                        color = if (page == pagerState.currentPage) Color.Gray else Color.LightGray
+                                    ) {}
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(12.dp))
+                            if (pagerState.currentPage < 5) {
+                                SharedConfirmButton(
+                                    text = "次へ",
+                                    onConfirm = { runBlocking { pagerState.scrollToPage(pagerState.currentPage + 1) } },
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center
+                                )
+                                Spacer(modifier = Modifier.height(12.dp))
+
                             }
                         }
                     }
