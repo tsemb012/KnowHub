ae119f0b1d627c512803e5f50a9e21ae982a6a19
tsemb012 <tsukasa0171015@gmail.com>
2023-08-09 23:06:39 +0900

    DST-746 年齢制限の下限を変更 (13歳)

--- app/src/main/java/com/tsemb/droidsoftthird/dialogs/BirthdayDialogFragment.kt
+++ app/src/main/java/com/tsemb/droidsoftthird/dialogs/BirthdayDialogFragment.kt
@@ -7,18 +7,23 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.time.LocalDate
 import java.util.Calendar
 import android.widget.NumberPicker
+import android.widget.Toast
 import com.tsemb.droidsoftthird.R
+import java.time.Period
 
 class BirthdayDialogFragment(private val onPositiveClickListener: (LocalDate) -> Unit) : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val maxEligibleYear = currentYear - 13 // 13歳以上を確実にするための年。
+
         val inflater = requireActivity().layoutInflater
         val dialogView = inflater.inflate(R.layout.dialog_birthday, null)
 
         val yearPicker = dialogView.findViewById<NumberPicker>(R.id.yearPicker).apply {
-            minValue = Calendar.getInstance().get(Calendar.YEAR) - 120
-            maxValue = Calendar.getInstance().get(Calendar.YEAR)
-            value = Calendar.getInstance().get(Calendar.YEAR) - 20 // Default selected year
+            minValue = currentYear - 100
+            maxValue = maxEligibleYear
+            value = currentYear - 20 // デフォルトの選択年。
         }
 
         val monthPicker = dialogView.findViewById<NumberPicker>(R.id.monthPicker).apply {
@@ -39,9 +44,21 @@ class BirthdayDialogFragment(private val onPositiveClickListener: (LocalDate) ->
                 val selectedMonth = monthPicker.value
                 val selectedDay = dayPicker.value
                 val birthday = LocalDate.of(selectedYear, selectedMonth, selectedDay)
-                onPositiveClickListener(birthday)
+
+                if (isAgeAbove13(birthday)) {
+                    onPositiveClickListener(birthday)
+                } else {
+                    Toast.makeText(requireContext(), "登録するには13歳以上である必要があります。", Toast.LENGTH_LONG).show()
+                }
             }
             .setNegativeButton(R.string.cancel, null)
             .create()
     }
+
+    private fun isAgeAbove13(birthday: LocalDate): Boolean {
+        val current = LocalDate.now()
+        val age = Period.between(birthday, current).years
+        return age >= 13
+    }
 }
+
